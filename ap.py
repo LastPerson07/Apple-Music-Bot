f0805b3af97121882a16485a76bf73e61e285dee
LastPerson07 <sharmadhanpal950@gmail.com>
2026-05-21 10:13:36 +0530

    Don't sync the repo

--- ap.py
+++ ap.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 APL_BASE = "https://aplmate.com"
 
-# Cloudflare
+# Cloudflare. // Don't sync the repo yet 
 HEADERS = {
     "User-Agent":        "Mozilla/5.0 (Linux; Android 14; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
     "Accept":            "*/*",
